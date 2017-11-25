651a1539145167d8e903948ac1bee745033cb000
Eric Marty <contact@ericmarty.co>
2017-11-24 18:18:17 -0800

    Movement - add physic material and sideways movement

--- Assets/PlayerMovement.cs
+++ Assets/PlayerMovement.cs
@@ -5,9 +5,45 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour {
 
 	public Rigidbody rb;
+
+	public float forwardForce = 2000f;
+	public float sidewaysForce = 500f;
+
+	bool moveLeft = false;
+	bool moveRight = false;
 	
+	void Update()
+	{
+		if (Input.GetKey("d"))
+		{
+			moveRight = true;
+		}
+		else
+		{
+			moveRight = false;
+		}
+		if (Input.GetKey("a"))
+		{
+			moveLeft = true;
+		}
+		else
+		{
+			moveLeft = false;
+		}
+	}
+
 	// Update is called once per frame
 	void FixedUpdate () {
-		rb.AddForce(0, 0, 2000 * Time.deltaTime);
+		rb.AddForce(0, 0, forwardForce * Time.deltaTime);
+
+		Debug.Log(moveRight);
+		if (moveRight)
+		{
+			rb.AddForce(sidewaysForce * Time.deltaTime, 0, 0);
+		}
+		if (moveLeft)
+		{
+			rb.AddForce(-sidewaysForce * Time.deltaTime, 0, 0);
+		}
 	}
 }
